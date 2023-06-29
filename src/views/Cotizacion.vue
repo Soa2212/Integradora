d6c0967f3963f8505acb982adf5d8b4d97c51b57
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-06-28 22:00:29 -0600

    Barra de navegacion, junto con ciertas vistas

---
+++ src/views/Cotizacion.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about">
+      <h1>Cotizacion Formulario</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
