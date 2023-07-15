3d0dba3c924f2070e1a6120a02b265296890a47b
Angel Tovar <angel.t069@outlook.com>
2023-07-15 10:55:48 -0600

    Esta es la vista del administrador que acabo de crear

---
+++ src/views/AdminView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(null);
+
+const links = [
+    ['mdi-inbox-arrow-down', 'Inbox'],
+    ['mdi-send', 'Send'],
+    ['mdi-delete', 'Trash'],
+    ['mdi-alert-octagon', 'Spam'],
+  ]
+</script>
+
+<template>
+      <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+        <v-sheet
+        color="grey-lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <template v-slot:prepend>
+            <v-icon>{{ icon }}</v-icon>
+          </template>
+
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+</style>
