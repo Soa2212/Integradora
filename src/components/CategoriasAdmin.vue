8d917da3e8bb096f73e17f59c88b54ba1df0eba1
Angel Tovar <angel.t069@outlook.com>
2023-07-16 14:02:11 -0600

    Estos son los nuevos componentes que aparecen en el router view del admin

---
+++ src/components/CategoriasAdmin.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hola buenas tardes, esta es la vista de las categorias</h1>
+</template>
---
+++ src/components/ProductosAdmin.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hola buenas tardes, esta es la vista de los productos</h1>
+</template>
