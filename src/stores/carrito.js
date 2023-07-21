bafe78eca74997be7507a0472d82509942a9c6c4
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-07-21 11:41:21 -0600

    El carrito se guarda en una variable local

--- src/stores/carrito.js
+++ src/stores/carrito.js
@@ -12,9 +12,18 @@ export const useCarritoStore = defineStore('carrito', {
   actions: {
     // Función para agregar un elemento al carrito
     agregarAlCarrito(item) {
-      this.carritoLS.push(item);
-      this.guardarCarritoEnLocalStorage();
-    },
+        const index = this.carritoLS.findIndex((producto) => producto.Articulo === item.Articulo);
+  
+        if (index !== -1) {
+          // Si el producto ya existe, actualizamos la cantidad del producto
+          this.carritoLS[index].cantidad = item.cantidad;
+        } else {
+          // Si el producto no existe, lo agregamos al carrito
+          this.carritoLS.push(item);
+        }
+  
+        this.guardarCarritoEnLocalStorage();
+      },
     // Otras acciones relacionadas con el carrito si las tienes
     // ...
     guardarCarritoEnLocalStorage() {
