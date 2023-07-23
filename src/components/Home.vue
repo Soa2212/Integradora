27d4212aa48ba988ec70f85234e58f28d864e02e
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-07-22 19:01:07 -0600

    Acomodando los productos que aparecen al inicio 1/3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,166 @@
-<script setup></script>
-<template>
-  <header class="header">
-    <section class="textos-header">
-      <h1
-        style="
-          width: 50%;
-          font-size: 25px;
-          margin-block-end: 15px;
-          color: rgb(255, 255, 255);
-        "
-      >
-        Equipo de Protección Personal para el Trabajo
-      </h1>
-      <h2 style="width: 50%; font-size: 20px; color: rgb(255, 255, 255)">
-        Somos proveedores de Equipo de protección personal para Industria,
-        contratistas, talleres y particulares. Encuentra el Equipo que necesitas
-        para tus proyectos teniendo como prioridad el cuidado de tu equipo de
-        colaboradores sin dejar de lado confort, accesibilidad y dinamismo en tu
-        logística para lograr óptimos resultados en todos los procesos de tu
-        Actividad.
-      </h2>
-    </section>
-    <div
-      class="wave"
-      style="height: 150px; margin-block-end: -3px; overflow: hidden"
-    >
-      <svg
-        viewBox="0 0 500 150"
-        preserveAspectRatio="none"
-        style="height: 100%; width: 100%"
-      >
-        <path
-          d="M-0.78,30.01 C290.97,148.12 349.20,-49.85 621.66,99.90 L500.00,149.60 L0.00,149.60 Z"
-          style="stroke: none; fill: rgb(245, 245, 220)"
-        ></path>
-      </svg>
-    </div>
-  </header>
+<script setup>
+import { ref, computed } from "vue";
+import { useCarritoStore } from "@/stores/carrito.js";
+const carritoStore = useCarritoStore();
 
-  <div class="container">
-    <div class="tarjetas">
-      <div>
-        <div class="productos" style="margin-top: 20px">
-          <h1 style="display: flex; justify-content: center; font-size: 24px">
-            Productos Recientes
-          </h1>
-          <v-row class="mt-3">
-            <v-col v-for="n in 12" :key="n" cols="4">
-              <v-card height="300" class="d-flex flex-column" style="cursor: pointer;">
+//Las tres primeras variables son para el popup cuando se agrega un producto al carrito o como yo le digo menu individual
+const hovered = ref(false);
+const isHovered = ref([]);
+const showPopup = ref(false);
+
+const productoPP = (id) => {
+  showPopup.value = true;
+  Articulos(id);
+};
+
+const productos = ref([]);
+fetch("http://localhost/categoriasp/2") //Me consigue todos los productos para mostrar en el catalogo
+  .then((res) => res.json())
+  .then((datos) => {
+    productos.value = datos.data;
+  });
+
+const ArticulosProd = ref([]);
+
+const Articulos = (id) => {
+  fetch("http://localhost/articulos/" + id)
+    .then((res) => res.json())
+    .then((datos) => {
+      ArticulosProd.value = datos.data;
+
+      // Verificar que ArticulosProd.value tenga al menos un objeto
+      if (ArticulosProd.value.length > 0) {
+        // Asignar las propiedades 'cantidad' y 'articulo' del primer objeto en ArticulosProd.value
+        amountArticulo.value = ArticulosProd.value[0].cantidad;
+        idArticulo.value = ArticulosProd.value[0].articulo;
+        if (ArticulosProd.value[0].tall_ropa != "No tiene") {
+          tallaArticulo.value = ArticulosProd.value[0].tall_ropa;
+        }
+        if (ArticulosProd.value[0].talla_numerica != "No tiene") {
+          tallaArticulo.value = ArticulosProd.value[0].talla_numerica;
+        }
+      }
+    });
+};
+
+const categorias = ref([]);
+fetch("http://localhost/categorias") //Me consigue todas las categorias para mostrar en la pagina
+  .then((res) => res.json())
+  .then((datos) => {
+    categorias.value = datos.data;
+  });
+
+function setHovered(index, value) {
+  isHovered.value[index] = value;
+}
+
+const productosConStock = computed(() => {
+  return productos.value.filter((producto) => producto.estado === "activo"); // Filtrador para evitar mostrar productos sin stock o si no estan activos
+});
+
+//El objeto de abajo y la funcion que le sigue es para mostra ciertas caracteristicas cuando se seleccione un producto
+const productBanner = ref({
+  nombre: "",
+  descripcion: "",
+  precio: "",
+  imagen1: null,
+});
+
+const productShow = (nombre, descripcion, precio, imagen1) => {
+  productBanner.value.nombre = nombre;
+  productBanner.value.descripcion = descripcion;
+  productBanner.value.precio = precio;
+  productBanner.value.imagen1 = imagen1;
+};
+
+const tallaArticulo = ref("No seleccionada");
+const idArticulo = ref(null);
+const amountArticulo = ref(null);
+</script>
+<template>
+  <div style="width: 100%; height: 100%">
+    <header class="header">
+      <section class="textos-header">
+        <h1
+          style="
+            width: 50%;
+            font-size: 25px;
+            margin-block-end: 15px;
+            color: rgb(255, 255, 255);
+          "
+        >
+          Equipo de Protección Personal para el Trabajo
+        </h1>
+        <h2 style="width: 50%; font-size: 20px; color: rgb(255, 255, 255)">
+          Somos proveedores de Equipo de protección personal para Industria,
+          contratistas, talleres y particulares. Encuentra el Equipo que
+          necesitas para tus proyectos teniendo como prioridad el cuidado de tu
+          equipo de colaboradores sin dejar de lado confort, accesibilidad y
+          dinamismo en tu logística para lograr óptimos resultados en todos los
+          procesos de tu Actividad.
+        </h2>
+      </section>
+      <div
+        class="wave"
+        style="height: 150px; margin-block-end: -3px; overflow: hidden"
+      >
+        <svg
+          viewBox="0 0 500 150"
+          preserveAspectRatio="none"
+          style="height: 100%; width: 100%"
+        >
+          <path
+            d="M-0.78,30.01 C290.97,148.12 349.20,-49.85 621.66,99.90 L500.00,149.60 L0.00,149.60 Z"
+            style="stroke: none; fill: rgb(245, 245, 220)"
+          ></path>
+        </svg>
+      </div>
+    </header>
+
+    <div class="container">
+      <div class="vista">
+        <div class="productos">
+          <p style="display: flex; justify-content: center; font-size: 26px">
+            Productos de una de nuestras categorias.
+          </p>
+          <div class="lh"></div>
+          <v-row class="mt-5">
+            <v-col
+              v-for="(producto, index) in productosConStock"
+              :key="producto.id"
+              cols="3"
+            >
+              <v-card
+                @mouseover="setHovered(index, true)"
+                @mouseleave="setHovered(index, false)"
+                height="360"
+                class="d-flex flex-column tarjeta"
+                style="box-shadow: 0 5px 15px -5px rgba(0, 0, 0, 2.9)"
+              >
                 <v-img
+                  style="border-radius: 10px"
                   height="100"
-                  src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+                  :src="producto.imagen1"
                   cover
-                >
-                </v-img>
-                <div class="informacion">
-                  <p>Bonita flor</p>
-                  <h3>Bonita flor de color morado</h3>
-                  <h2>$1399</h2>
+                ></v-img>
+                <div class="informacion" :class="{ hovered: isHovered[index] }">
+                  <button>
+                    <p style="font-size: 15px; text-align: center">
+                      {{ producto.categoria }}
+                    </p>
+                    <h3
+                      style="
+                        cursor: pointer;
+                        font-size: 15px;
+                        text-align: center;
+                      "
+                    >
+                      {{ producto.nombre }}
+                    </h3>
+                    <h2 style="font-size: 15px; text-align: center">
+                      $ {{ producto.precio }}
+                    </h2>
+                  </button>
                 </div>
               </v-card>
             </v-col>
@@ -74,8 +177,327 @@
   font-family: "Montserrat", sans-serif;
   font-size: 17px;
 }
+.contenedor {
+  background-color: beige;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 50px;
+  width: 100%;
+  height: min-content;
+}
+
+.categorias {
+  width: 30%;
+  height: max-content;
+  padding-bottom: 170px;
+  margin-right: 10px;
+  background-color: white;
+  padding-left: 15px;
+  padding-top: 10px;
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
+}
+
+.CB {
+  background-color: white;
+}
+
+.lh {
+  border-bottom: 1px solid #ccc;
+  margin-bottom: 10px;
+  border-radius: 50px;
+  width: 90%;
+  height: 5px;
+}
+.CTB {
+  font-size: 26px;
+  text-align: center;
+  margin-top: 25px;
+  margin-left: 70px;
+}
+
+.CTB:hover {
+  /* Sombras al hacer hover */
+  color: rgb(28, 172, 216);
+  transition: 0.5s;
+}
+
+.CBB {
+  color: #837f7f;
+  margin-top: 7px;
+  transition: 0.3s;
+}
+
+.CBB:hover {
+  /* Sombras al hacer hover */
+  color: rgb(0, 0, 0);
+  font-size: 24px;
+  transition: 0.6s;
+}
+.productos {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-top-right-radius: 20px; /* Radio de esquina superior derecha */
+  border-bottom-right-radius: 20px; /* Radio de esquina inferior derecha */
+  padding: 2em 1em;
+}
+
+.tarjeta {
+  border-radius: 15px;
+  position: relative;
+  overflow: hidden;
+  transition: transform 0.5s;
+}
+
 .informacion {
+  transition: transform 0.5s;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+}
+
+.informacion.hovered {
+  box-shadow: 0 -10px 10px rgba(128, 128, 128, 0.5);
+  transition: 0.5s;
+  transform: translateY(-25%);
+  border-radius: 30px;
+}
+
+.informacion.hovered p {
+  margin-top: 40px;
+}
+s .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.popup-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.popup-content {
+  background-color: white;
+  display: flex;
+  width: 55%;
+  height: 67%;
+  padding: 20px;
+  border-radius: 15px;
+  position: relative; /* Añadido para posicionar el botón de cierre */
+}
+
+.close-button {
+  position: absolute;
+  top: -30px;
+  right: -30px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 14px;
+  z-index: 1;
+  font-size: 20px;
+  color: white;
+}
+
+.close-button:hover {
+  color: black;
+  transition: 0.55s;
+}
+
+.linea {
+  width: 1px;
+  background-color: rgba(193, 180, 180, 0.76);
+}
+.CTPP {
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+  color: gray;
+  font-size: 13px;
+}
+.botnsP {
+  font-size: 15px;
+  margin-top: 10px;
+  margin-block-end: 10px;
+}
+
+.botnsEst {
+  width: 80%;
+  margin-left: 20px;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  border: solid 2px black;
+  margin-top: 10px;
+}
+
+.botnsEstF {
+  width: 80%;
+  margin-left: 20px;
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  background-color: black;
+  color: white;
+}
+
+.botnsEst:hover {
+  color: white;
+  background-color: black;
+  transition: 0.5s;
+}
+.inpQA {
+  width: 90px;
+  height: 50px;
+  margin-top: 15px;
+  margin-block-end: 5px;
+  outline: none;
+  text-align: center;
+  background-color: #f7f8fa;
+}
+
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type="number"] {
+  -moz-appearance: textfield; /* Para Firefox */
+}
+
+.btnA {
+  font-size: 25px;
+  font-weight: 900;
+  margin-top: 15px;
+  margin-block-end: 5px;
+  background-color: #f7f8fa;
+  height: 50px;
+  color: #777777;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btnA:hover {
+  color: black;
+}
+
+.descArt {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 14px;
+  line-height: 22px;
+  color: #777777;
+}
+
+.btnTallas {
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  background-color: #f7f8fa;
+}
+
+.btnTallas:hover {
+  background-color: black;
+  color: white;
+  transition: 0.5s;
+}
+
+.scroll {
+  /* Estilo para el div que contendrá los botones de las tallas */
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 10px;
+  align-items: center;
+  margin-left: 10px;
+  overflow-x: auto;
+  max-height: 200px;
+  flex-wrap: nowrap;
+  padding-right: 5px;
+}
+
+.scroll button {
+  /* Estilo para los botones de las tallas */
+  width: 40px; /* Establece el ancho deseado para los botones */
+  flex-shrink: 0; /* Evita que los botones se reduzcan si hay muchos en una fila */
+}
+
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 7%;
+}
+
+.modal-contenido {
+  background-color: #f44336;
+  color: white;
+  top: 50%;
+  left: 50%;
   padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+/* Transición para la pestaña emergente */
+.pestanita-enter-active,
+.pestanita-leave-active {
+  transition: opacity 0.3s;
+}
+
+.pestanita-enter, .pestanita-leave-to /* .pestanita-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+
+.BannerNP {
+  margin-top: 10px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.BannerIO {
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  font-weight: bold;
+  font-style: oblique;
+}
+
+.linea-horizontal {
+  margin-top: -30px;
+  border-bottom: 1px solid #ccc;
+  border-radius: 50px;
+  height: 5px;
+}
+
+.informacion {
+  transition: transform 0.5s;
+  background-color: white;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -103,6 +525,7 @@
 .header {
   width: 100%;
   height: 450px;
+  margin-top: -20px;
   background: #004ff9; /* fallback for old browsers */
   background: -webkit-linear-gradient(
       to right,
@@ -120,6 +543,20 @@
   background-attachment: fixed;
   background-repeat: no-repeat;
   position: relative;
+}
+
+.tarjeta {
+  border-radius: 15px;
+  position: relative;
+  overflow: hidden;
+  transition: transform 0.5s;
+}
+
+.vista {
+  display: flex;
+  width: 80vw;
+  height: 90%;
+  background-color:#004ff9 ;
 }
 
 .textos-header {
@@ -183,5 +620,14 @@
   width: 75%;
   display: flex;
   flex-direction: column;
+}
+
+.productos {
+  width: 75%;
+  height: 100%;
+  background-color: white;
+  border-top-right-radius: 20px; /* Radio de esquina superior derecha */
+  border-bottom-right-radius: 20px; /* Radio de esquina inferior derecha */
+  padding: 2em 1em;
 }
 </style>
