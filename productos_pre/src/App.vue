859d85eb1230356238eb98eb30b76bc9d9c6cf67
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-07-24 12:43:57 -0600

    Elimacion de archivos de de un repositoio

--- productos_pre/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div style="display: flex; justify-content: center;"><h1>Aqui va el menu</h1></div>
-<cProductos/>
-</template>
-
-<script setup>
-  import cProductos from './components/cProductos.vue';
-</script>
