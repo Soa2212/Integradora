141df8482538549092d2b291d06b77e5e77f6918
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-08-09 19:10:01 -0600

    waza

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '',
-    redirect: '/HomeView', // Redireccionar la ruta raíz a la página de inicio
+    path: "",
+    redirect: "/HomeView", // Redireccionar la ruta raíz a la página de inicio
   },
   {
-    path: '/HomeView',
-    name: 'HomeView',
-    component: () => import('@/views/HomeView.vue'),
-  },
-  { 
-    path:'/ProductosView',
-    name:'ProductosView',
-    component:()=>import('@/views/ProductosView.vue')
+    path: "/HomeView",
+    name: "HomeView",
+    component: () => import("@/views/HomeView.vue"),
   },
   {
-    path:'/LoginView',
-    name:'LoginView',
-    component:()=>import('@/views/LoginView.vue')
+    path: "/ProductosView",
+    name: "ProductosView",
+    component: () => import("@/views/ProductosView.vue"),
   },
   {
-    path:'/AcercaView',
-    name:'AcercaView',
-    component:()=>import('@/views/AcercaView.vue')
+    path: "/LoginView",
+    name: "LoginView",
+    component: () => import("@/views/LoginView.vue"),
   },
   {
-    path:'/Registro',
-    name:'Registro',
-    component:()=>import('@/views/RegistroView.vue')
+    path: "/AcercaView",
+    name: "AcercaView",
+    component: () => import("@/views/AcercaView.vue"),
+  },
+  {
+    path: "/Registro",
+    name: "Registro",
+    component: () => import("@/views/RegistroView.vue"),
   },
   {
     path: "/Carrito",
@@ -37,38 +37,30 @@ const routes = [
     component: () => import("@/views/CarritoView.vue"),
   },
   {
-    path:'/AdminHome',
-    name:'AdminHome',
-    component:()=>import('@/components/AdminHome.vue')
+    path: "/AdminHome",
+    name: "AdminHome",
+    component: () => import("@/components/AdminHome.vue"),
   },
   {
-    path:'/AdminProductos',
-    name:'AdminPro',
-    component:()=>import('@/components/ProductosAdmin.vue')
+    path: "/AdminProductos",
+    name: "AdminPro",
+    component: () => import("@/components/ProductosAdmin.vue"),
   },
   {
-    path:'/AdminCategorias',
-    name:'AdminCat',
-    component:()=>import('@/components/CategoriasAdmin.vue')
+    path: "/AdminCategorias",
+    name: "AdminCat",
+    component: () => import("@/components/CategoriasAdmin.vue"),
   },
   {
-    path:'/AgregarAdmin',
-    name:'AdminAdd',
-    component:()=>import('@/components/AgregarUsuarioAdmin.vue')
+    path: "/AgregarAdmin",
+    name: "AdminAdd",
+    component: () => import("@/components/AgregarUsuarioAdmin.vue"),
   },
-  {
-    path:'/Pedido',
-    name:'Pedido',
-    component:()=>import('@/components/PedidoC.vue')
-  },
-  
-]
-  
-
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
