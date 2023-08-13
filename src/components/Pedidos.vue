e5af7c7492b37ea96314a18b082b960b7b8f2412
JesusAr544 <aranda5446373773544@gmail.com>
2023-08-12 20:30:18 -0600

    peticiones pra historial de pedidos

--- src/components/Pedidos.vue
+++ src/components/Pedidos.vue
@@ -1,11 +1,40 @@
 <script setup>
+import { ref } from 'vue';
 import {useIdStore} from "@/stores/IdUSR";
 const Idusr = useIdStore();
 const Id = Idusr.getIdLS;
+let compra=ref([]);
+let detalle=ref([]);
+let objeto =ref({
+id :'',
+ordenid :''
+});
+objeto.value.id=Id;
+fetch('http://localhost/comprasusuario',{
+    method: 'POST',
+    body: JSON.stringify(objeto.value)
+})
+.then((response)=>response.json())
+.then(data=>compra.value=data.data)
+const detalles =(or)=>{
+objeto.value.ordenid=or;
+  fetch('http://localhost/detallecompra',{
+    method: 'POST',
+    body: JSON.stringify(objeto.value)
+})
+.then((response)=>response.json())
+.then(data=>detalle.value=data.data)
+}
 </script>
 <template>
 <div class="contenedor">
-    <div class="tarjeta">{{ Id }}</div>
+    <div class="tarjeta">{{ Id }}
+    {{ compra }}
+    <button type="button" @click="detalles(1)">ajjssa</button>
+    {{ detalle }}
+    
+    
+    </div>
 </div>
 </template>
 <style scoped>
