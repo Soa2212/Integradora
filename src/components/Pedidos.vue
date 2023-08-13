4c1c87c1e7c6c4b6a143b7971fbf71aea5b0be37
JesusAr544 <aranda5446373773544@gmail.com>
2023-08-12 21:07:17 -0600

    componenente con con estructura

--- src/components/Pedidos.vue
+++ src/components/Pedidos.vue
@@ -26,13 +26,111 @@ objeto.value.ordenid=or;
 .then(data=>detalle.value=data.data)
 }
 </script>
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+}
+</script>
 <template>
 <div class="contenedor">
-    <div class="tarjeta">{{ Id }}
-    {{ compra }}
-    <button type="button" @click="detalles(1)">ajjssa</button>
-    {{ detalle }}
-    
+    <div class="tarjeta">
+        <div> <v-table theme="width">
+      <thead>
+        <tr>
+          <th class="text-left">
+              #Orden
+            </th>
+            <th class="text-left">
+              fecha
+            </th>
+        
+            <th class="text-left">
+              cantidad de articulos
+            </th>
+            <th class="text-left">
+              estado
+            </th>
+            <th class="text-left">
+              total
+            </th>
+          
+        </tr>
+      </thead>
+      <tbody v-for="item in compra"
+          :key="item.orden">
+        <tr>
+        <td>{{ item.orden }}</td>
+            <td>{{ item.fecha }}</td>
+            <td>{{ item.cantidad_articulos }}</td>
+            <td>{{ item.estado }}</td>
+            <td>{{ item.total }}</td>
+            <v-btn
+          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          @click="show = !show , detalles(item.orden)"
+        ></v-btn>
+          
+        </tr>
+       
+      
+  
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <div> <v-table theme="width">
+      <thead>
+        <tr>
+         
+            <th class="text-left">
+              Producto
+            </th>
+            <th class="text-left">
+              Talla_N
+            </th>
+            <th class="text-left">
+              Talla_R
+            </th>
+            <th class="text-left">
+              color
+            </th>
+            <th class="text-left">
+              precio
+            </th>
+            <th class="text-left">
+              cantidad
+            </th>
+            <th class="text-left">
+              total
+            </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in detalle"
+          :key="item.producto"
+        >
+        
+            <td>{{ item.producto }}</td>
+            <td>{{ item.TALLA_NUMERICA }}</td>
+            <td>{{ item.TALLA_ROPA }}</td>
+            <td>{{ item.color }}</td>
+            <td>{{ item.precio_unitario }}</td>
+            <td>{{ item.cantidad}}</td>
+            <td>{{ item.total}}</td>
+            <td><img :src="item.imagen" alt=""></td>
+        </tr>
+      </tbody>
+    </v-table></div>
+          
+        </div>
+      </v-expand-transition>
+
+
+
+      </tbody>
+    </v-table></div>
+
     
     </div>
 </div>
