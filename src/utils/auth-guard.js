7ee1b35784f535d99eafa1341c4f3c05d5eabca9
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-08-18 11:55:14 -0600

    upd1

--- src/utils/auth-guard.js
+++ src/utils/auth-guard.js
@@ -1,15 +1,14 @@
 import { useTipoStore } from "@/stores/TipoUSR";
-import { useTokenStore } from "@/stores/TokenUser"; // Importa tu tienda de tokens
+import { useTokenStore } from "@/stores/TokenUser";
 
 export function isAdmin(to, from, next) {
   const TipoUSR = useTipoStore();
-  const Token = useTokenStore();
-  const Admin = computed(() => TipoUSR.tieneTipo);
-  
-  const currentUser = Token.tieneToken(); // Usa la función tieneToken() de tu tienda de tokens
-  const tieneTipoAdmin = TipoUSR.tieneTipo(); // Utiliza la función tieneTipo() para verificar si el usuario es admin o dueño
 
-  if (currentUser && tieneTipoAdmin) {
+  const tokenStore = useTokenStore();
+  const tieneToken = tokenStore.tieneToken;
+  const tieneTipoAdmin = TipoUSR.tieneTipo;
+
+  if (tieneToken && tieneTipoAdmin) {
     next();
   } else {
     next("/HomeView");
